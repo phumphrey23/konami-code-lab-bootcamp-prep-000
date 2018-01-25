8bb40662246396bdc1c98122073b29982834e66d
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-25 03:10:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,9 +3,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   document.addEventListener('keydown', function (e) {
     var index = 0;
-  const key = parseInt(e.detail || e.which);
-  if(key === code[index]){
-    index++;
+    const key = parseInt(e.detail || e.which);
+    if(key === code[index]){
+    index++
      if(index === code.length-1){
        alert("Hurray!");
        index = 0;
